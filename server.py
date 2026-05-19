cbcc8dc093ebd173ca4470d66030fa5bcca7a24f
Guilpos <guilherme.soucampos@gmail.com>
2026-05-19 12:51:36 -0300

    Modo desenvolvimento False

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ from python.Rf1 import RF1
 
 app = FastAPI()
 # Mude para False quando subir para produção
-MODO_DESENVOLVIMENTO = True 
+MODO_DESENVOLVIMENTO = False 
 
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
